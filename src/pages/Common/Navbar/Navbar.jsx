ebbad8c212f30a9af5c6fbc13c191490aa73a0dc
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2024-12-11 19:34:44 +0600

    implement log in page ui

--- src/pages/Common/Navbar/Navbar.jsx
+++ src/pages/Common/Navbar/Navbar.jsx
@@ -1,5 +1,91 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/icons8-job-seeker-100.png";
+import { AuthContext } from "./../../../Provider/AuthContext/AuthContext";
+
 const Navbar = () => {
-  return <div></div>;
+  const { user } = useContext(AuthContext);
+  const links = (
+    <>
+      {user ? (
+        <>
+          <button>Sign Out</button>
+        </>
+      ) : (
+        <>
+          <li className=" hover:-translate-y-[2px] duration-200 hover:text-[#EE552A] underline ">
+            <Link to={"/register"} className="!bg-transparent">
+              Register
+            </Link>
+          </li>
+          <li className="relative">
+            <Link
+              to={"/login"}
+              className="hover:-translate-y-1 rounded-full duration-200 btn btn-sm bg-[#EE552A] hover:btn-outline text-black hover:!text-[#EE552A] hover:!bg-transparent hover:!border-[#EE552A]"
+            >
+              Log in
+            </Link>
+          </li>
+        </>
+      )}
+    </>
+  );
+  return (
+    <>
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="navbar bg-base-300 w-full px-5 sm:px-10 lg:px-20">
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="mx-2 flex-1 px-2">
+              <Link to={"/"} className="flex items-center gap-1 ">
+                <img src={logo} className="w-10" alt="" />{" "}
+                <span className="text-lg font-bold">A Job Portal</span>
+              </Link>
+            </div>
+            <div className="hidden flex-none lg:block">
+              <ul className="menu menu-horizontal">
+                {/* Navbar menu content here */}
+                {links}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            {links}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,5 +1,79 @@
+import Lottie from "lottie-react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import animation from "..//..//assets/Lotie/register.json";
 const LogIn = () => {
-  return <div></div>;
+  const loginFormHandler = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log({ email, password });
+  };
+  return (
+    <>
+      <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className=" lg:text-left">
+            <Lottie className="w-full" animationData={animation} />
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 duration-150 border-[#EE552A] hover:shadow-2xl ">
+            <div className="text-center mt-5">
+              <h1 className="text-4xl font-bold">Login now!</h1>
+            </div>
+            <form onSubmit={loginFormHandler} className="p-5">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered rounded-full"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered rounded-full"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn hover:-translate-y-[2px] hover:bg-[#EE552A] bg-[#FFAD7B] text-white rounded-full hover:shadow-2xl transition-all duration-200">
+                  Login
+                </button>
+              </div>
+            </form>
+            <div className="p-5 pt-0">
+              <div className="divider"> OR</div>
+              <button className="btn hover:-translate-y-[2px] !bg-white hover:shadow transition-all duration-200 hover:rounded-full w-full flex">
+                <FcGoogle size={25} /> <span>Log in with Google</span>
+              </button>
+              <p className="my-3 text-sm">
+                Don't have an account?{" "}
+                <Link to="/register" className="underline hover:text-[#EE552A]">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LogIn;
