5a47a75ae157aa956731252f97b46c7cb32df1a5
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2024-12-14 19:06:12 +0600

    added job delete functionality

--- src/pages/myJOb/MyJob.jsx
+++ src/pages/myJOb/MyJob.jsx
@@ -1,12 +1,52 @@
 import React, { useState } from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyJob = () => {
   const { data } = useLoaderData();
+  const { email } = useParams();
   const [jobs, setJobs] = useState(data ? data : []);
 
-  const deleteHandler = (id) => {
-    console.log(id);
+  const makeSure = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteHandler(id);
+      }
+    });
+  };
+
+  const deleteHandler = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:5000/jobs/${id}`, {
+        method: "DELETE",
+        headers: { "content-type": "application/json" },
+      });
+      if (res.status === 200) {
+        fetch(`http://localhost:5000/jobs?hr_email=${email}`)
+          .then((res) => res.json())
+          .then((data) => setJobs(data.data));
+
+        Swal.fire({
+          title: "Deleted success",
+          timer: 2000,
+          showConfirmButton: false,
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        title: error.message,
+        timer: 2000,
+        showConfirmButton: false,
+      });
+    }
   };
 
   if (jobs.length === 0) return <h2>No Job found</h2>;
@@ -59,7 +99,7 @@ const MyJob = () => {
                   </Link>
                   <button
                     className="btn btn-xs text-error "
-                    onClick={() => deleteHandler(job._id)}
+                    onClick={() => makeSure(job._id)}
                   >
                     Delete
                   </button>
